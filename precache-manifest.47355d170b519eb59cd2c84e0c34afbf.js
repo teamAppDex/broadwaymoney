c76960ab5ab674bc2b0e8356f8ad3774d7a32908
Application Design <my.appdex@gmail.com>
2020-04-04 20:50:46 +0100

    Updates

---
+++ precache-manifest.47355d170b519eb59cd2c84e0c34afbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8aaf42ecd7953d72d16713ff42aa34",
+    "url": "/broadwaymoney/index.html"
+  },
+  {
+    "revision": "4d47abed4c6a86a1b757",
+    "url": "/broadwaymoney/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d95c48e593c5ea556545",
+    "url": "/broadwaymoney/static/js/2.66bee83f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/broadwaymoney/static/js/2.66bee83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d47abed4c6a86a1b757",
+    "url": "/broadwaymoney/static/js/main.907afe20.chunk.js"
+  },
+  {
+    "revision": "1a37261a8523dadbdbe6",
+    "url": "/broadwaymoney/static/js/runtime-main.4cf0d750.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/broadwaymoney/static/media/logo.5d5d9eef.svg"
+  }
+]);
